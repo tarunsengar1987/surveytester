38772b7008fff7d1c46235d0cf1fc82ca7434a60
Ajay <ajaym.brainerhub@gmail.com>
2021-07-07 16:55:58 +0530

    SUT2021-14 - Project Tree - Details

--- src/00-surveytester/appbar/appbar.tsx
+++ src/00-surveytester/appbar/appbar.tsx
@@ -9,6 +9,10 @@ import MenuIcon from "@material-ui/icons/Menu";
 import { TreeView, TreeItem } from "@material-ui/lab";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import { withStyles } from '@material-ui/core/styles';
+import MuiAccordion from '@material-ui/core/Accordion';
+import MuiAccordionSummary from '@material-ui/core/AccordionSummary';
+import MuiAccordionDetails from '@material-ui/core/AccordionDetails';
 import { CompanyModel } from "../../model/company.model";
 import { FolderModel } from "../../model/folder.model";
 import { ProjectModel } from "../../model/project.model";
@@ -47,6 +51,44 @@ const useStyles = makeStyles((theme) =>
     },
   })
 );
+
+const Accordion = withStyles({
+  root: {
+    border: '1px solid rgba(0, 0, 0, .125)',
+    boxShadow: 'none',
+    '&:not(:last-child)': {
+      borderBottom: 0,
+    },
+    '&:before': {
+      display: 'none',
+    }
+  },
+  expanded: {},
+})(MuiAccordion);
+
+const AccordionSummary = withStyles({
+  root: {
+    backgroundColor: 'rgba(0, 0, 0, .03)',
+    borderBottom: '1px solid rgba(0, 0, 0, .125)',
+    marginBottom: -1,
+    minHeight: 56,
+    '&$expanded': {
+      minHeight: 56,
+    },
+  },
+  content: {
+    '&$expanded': {
+      margin: '0px',
+    },
+  },
+  expanded: {},
+})(MuiAccordionSummary);
+
+const AccordionDetails = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiAccordionDetails);
 
 export default function Topbar() {
   const classes = useStyles()
@@ -89,44 +131,107 @@ export default function Topbar() {
     );
   };
 
+  const [expanded, setExpanded] = useState('panel1');
+
+  const handleChange = (panel: any) => (event: any, newExpanded: any) => {
+    setExpanded(newExpanded ? panel : false);
+  };
+
   return (
     <div className="topbar">
       <div className="topbarWrapper">
         <CssBaseline />
         <Drawer open={open} onClose={() => setOpen(false)}>
-          {companies && (
-            <TreeView
-              className={classes.drawer}
-              defaultCollapseIcon={<ExpandMoreIcon />}
-              defaultExpanded={["root"]}
-              defaultExpandIcon={<ChevronRightIcon />}
-            >
-              {companies.map((company: CompanyModel) => (
-                <TreeItem
-                  label={company.CompanyName}
-                  nodeId={company.IdCompany}
-                  key={company.IdCompany}
+          <Accordion square expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+            <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+              <Typography>Favorite Projects</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              {companies && (
+                <div>
+                  {companies.map((company: CompanyModel) => (
+                    <>
+                      {company.FavoriteProjects.map((project: ProjectModel) =>
+                        renderProjectName(project.ProjectName, project.IdProject)
+                      )}
+                    </>
+                  ))}
+                </div>
+              )}
+            </AccordionDetails>
+          </Accordion>
+          <Accordion square expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+            <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
+              <Typography>Recent Projects</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              {companies && (
+                <div>
+                  {companies.map((company: CompanyModel) => (
+                    <span>
+                      {company.RecentProjects.map((project: ProjectModel) =>
+                        renderProjectName(project.ProjectName, project.IdProject)
+                      )}
+                    </span>
+                  ))}
+                </div>
+              )}
+            </AccordionDetails>
+          </Accordion>
+          <Accordion square expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+            <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
+              <Typography>All Projects</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              {companies && (
+                <TreeView
+                  className={classes.drawer}
+                  defaultCollapseIcon={<ExpandMoreIcon />}
+                  defaultExpanded={["root"]}
+                  defaultExpandIcon={<ChevronRightIcon />}
                 >
-                  <TreeItem nodeId="all projects" label="All Projects">
-                    {company.AllProjects.map((allProject: FolderModel) =>
-                      renderFolder(allProject)
-                    )}
-                  </TreeItem>
-                  <TreeItem nodeId="recent" label="Recent Projects">
-                    {company.RecentProjects.map((project: ProjectModel) =>
-                      renderProjectName(project.ProjectName, project.IdProject)
-                    )}
-                  </TreeItem>
-                  <TreeItem nodeId="favorite" label="Favorite Projects">
-                    {company.FavoriteProjects.map((project: ProjectModel) =>
-                      renderProjectName(project.ProjectName, project.IdProject)
-                    )}
-                  </TreeItem>
-                  <TreeItem nodeId="closed" label="Closed Projects" />
-                </TreeItem>
-              ))}
-            </TreeView>
-          )}
+                  {companies.map((company: CompanyModel) => (
+                    <TreeItem
+                      label={company.CompanyName}
+                      nodeId={company.IdCompany}
+                      key={company.IdCompany}
+                    >
+                      {company.AllProjects.map((allProject: FolderModel) =>
+                        renderFolder(allProject)
+                      )}
+                    </TreeItem>
+                  ))}
+                </TreeView>
+              )}
+            </AccordionDetails>
+          </Accordion>
+          <Accordion square expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+            <AccordionSummary aria-controls="panel4`d-content" id="panel4d-header">
+              <Typography>Closed Projects</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              {companies && (
+                <TreeView
+                  className={classes.drawer}
+                  defaultCollapseIcon={<ExpandMoreIcon />}
+                  defaultExpanded={["root"]}
+                  defaultExpandIcon={<ChevronRightIcon />}
+                >
+                  {companies.map((company: CompanyModel) => (
+                    <TreeItem
+                      label={company.CompanyName}
+                      nodeId={company.IdCompany}
+                      key={company.IdCompany}
+                    >
+                      {company.ClosedProjects.map((allProject: FolderModel) =>
+                        renderFolder(allProject)
+                      )}
+                    </TreeItem>
+                  ))}
+                </TreeView>
+              )}
+            </AccordionDetails>
+          </Accordion>
         </Drawer>
         <AppBar position="static" color="primary">
           <Toolbar>
