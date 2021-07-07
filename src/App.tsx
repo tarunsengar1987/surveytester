93d4f8c3951ab748186bd762fcda1077fc0af817
Ajay <ajaym.brainerhub@gmail.com>
2021-07-07 11:23:27 +0530

    SUT2021-18 : The filename needs to be dashboard.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Topbar from "./00-surveytester/topbar";
 import Login from "./01-login/login";
-import Dashboard from "./02-dashboard";
+import Dashboard from "./02-dashboard/dashboard";
 import "./App.scss";
 
 function App() {
