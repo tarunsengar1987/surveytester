d9342753b708cade1fcc3a1b484efbab71aacc44
Rahul <rahulv.brainerhub@gmail.com>
2021-07-09 12:02:34 +0530

    SUT2021-30 : I forgot my Password

--- src/01-login/Login/login.tsx
+++ src/01-login/Login/login.tsx
@@ -1,33 +1,45 @@
 import { useState } from "react";
-import { useHistory } from "react-router-dom"
-import axios from "axios"
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 import {
-  Button, TextField, FormControlLabel, Checkbox, Link, Grid, Dialog,
-  DialogActions, DialogContent, DialogContentText, DialogTitle
-} from '@material-ui/core';
+  Button,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Link,
+  Grid,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@material-ui/core";
 import { toast } from "react-toastify";
 import "../login.scss";
 import { useTranslation } from "react-i18next";
 import i18next from "i18next";
 const Login = () => {
-
   const SERVER_URL = process.env.REACT_APP_SERVER;
   const history = useHistory();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [open, setOpen] = useState(false);
   const { t } = useTranslation();
+  const [forgetEmail, setForgetEmail] = useState("");
 
   const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    const { data } = await axios.post(SERVER_URL + 'API/V2/authentication.ashx?method=login', {
-      email: email,
-      password: password,
-      timestamp: new Date().toUTCString()
-    })
+    event.preventDefault();
+    const { data } = await axios.post(
+      SERVER_URL + "API/V2/authentication.ashx?method=login",
+      {
+        email: email,
+        password: password,
+        timestamp: new Date().toUTCString(),
+      }
+    );
 
     if (data.Token) {
-      localStorage.setItem("userData", JSON.stringify(data))
+      localStorage.setItem("userData", JSON.stringify(data));
       toast.success(t("login.loginSuccessfully"));
       history.push("/dashboard");
     } else {
@@ -35,16 +47,48 @@ const Login = () => {
     }
   };
 
+  const handleSend = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const { data } = await axios.post(
+      SERVER_URL + "api/v2/authentication.ashx?method=recoverPassword",
+      {
+        email: forgetEmail,
+        language:
+          localStorage.getItem("i18nextLng") === null ||
+          localStorage.getItem("i18nextLng") === "en"
+            ? "English"
+            : "German",
+        timestamp: new Date().toUTCString(),
+      }
+    );
+
+    if (data.Status.Status === "OK") {
+      toast.success(t("login.emailSendSucessful"));
+    } else {
+      toast.error(t("login.emailIncorrect"));
+    }
+    setOpen(false);
+    setForgetEmail("");
+  };
+
   return (
     <div className="main-bg">
       <div className="login-new">
         <Grid container>
           <Grid item xs>
-            <Link href="#" variant="body1" onClick={() => i18next.changeLanguage('en')}>
+            <Link
+              href="#"
+              variant="body1"
+              onClick={() => i18next.changeLanguage("en")}
+            >
               English
             </Link>
             <span> | </span>
-            <Link href="#" variant="body2" onClick={() => i18next.changeLanguage('de')}>
+            <Link
+              href="#"
+              variant="body2"
+              onClick={() => i18next.changeLanguage("de")}
+            >
               German
             </Link>
           </Grid>
@@ -77,36 +121,9 @@ const Login = () => {
             onChange={(e) => setPassword(e.target.value)}
           />
           <div>
-            <Link className="cursor-pointer" onClick={() => setOpen(true)}>{t("login.forgotPassword")}</Link>
-            <Dialog
-              open={open}
-              onClose={() => setOpen(false)}
-              aria-labelledby="form-dialog-title"
-            >
-              <DialogTitle id="form-dialog-title">{t("login.passwordRecovery")} </DialogTitle>
-              <DialogContent>
-                <DialogContentText>
-                  {t("login.forgotPasswordText")}
-                </DialogContentText>
-                <TextField
-                  autoFocus
-                  margin="dense"
-                  id="name"
-                  label={t("login.emailAddress")}
-                  type="email"
-                  fullWidth
-                />
-              </DialogContent>
-              <DialogActions>
-                <Button onClick={() => setOpen(false)} color="primary">
-                  {t("cancel")}
-                </Button>
-                {/* // TODO: Reset Password functionality*/}
-                <Button onClick={() => setOpen(false)} color="primary">
-                  {t("send")}
-                </Button>
-              </DialogActions>
-            </Dialog>
+            <Link className="cursor-pointer" onClick={() => setOpen(true)}>
+              {t("login.forgotPassword")}
+            </Link>
           </div>
           {/* // TODO: Keep me signed in functionality*/}
           <FormControlLabel
@@ -114,7 +131,14 @@ const Login = () => {
             label={t("login.keepMeSignedIn")}
           />
           <div className="buttons">
-            <Button name="signin" value="Login" type="submit" id="signin" className="btn btn-green" color="primary">
+            <Button
+              name="signin"
+              value="Login"
+              type="submit"
+              id="signin"
+              className="btn btn-green"
+              color="primary"
+            >
               {t("login.signIn")}
             </Button>
           </div>
@@ -153,7 +177,41 @@ const Login = () => {
           </ul>
         </form>
       </div>
-    </div >
+      <Dialog
+        open={open}
+        onClose={() => setOpen(false)}
+        aria-labelledby="form-dialog-title"
+      >
+        <form onSubmit={(event) => handleSend(event)}>
+          <DialogTitle id="form-dialog-title">
+            {t("login.passwordRecovery")}{" "}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              {t("login.forgotPasswordText")}
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              label={t("login.emailAddress")}
+              type="email"
+              required
+              fullWidth
+              value={forgetEmail}
+              onChange={(e) => setForgetEmail(e.target.value)}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => setOpen(false)} color="primary">
+              {t("cancel")}
+            </Button>
+            <Button color="primary" type="submit">
+              {t("send")}
+            </Button>
+          </DialogActions>
+        </form>
+      </Dialog>
+    </div>
   );
 };
 export default Login;
